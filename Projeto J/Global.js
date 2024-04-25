4ba112d4df9c57e7b9fcc2d399b386a885f21c78
LeticiaKobus <146870615+LeticiaKobus@users.noreply.github.com>
2024-04-25 18:45:56 -0300

    Delete Projeto J directory

--- Projeto J/Global.js
+++
@@ -1,6 +0,0 @@
-function dimensaoTela() {
-  var heightTela = window.innerHeight;
-  var widthTela = window.innerWidth;
-
-  document.getElementById("pag_processos").style.minHeight = heightTela + "px";
-}
